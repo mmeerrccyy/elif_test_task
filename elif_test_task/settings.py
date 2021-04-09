e7233408c25c76b278797b31bccf2944d7406a12
mmeerrccyy <taras.mercury@gmail.com>
2021-04-09 18:14:20 +0300

    migrations

--- elif_test_task/settings.py
+++ elif_test_task/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'elif_test_task.urls'
